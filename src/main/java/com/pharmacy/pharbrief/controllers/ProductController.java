2115f04d5a8ea184b7f497396da0232d32ef4a47
oussamasine07 <sineoussama@gmail.com>
2025-05-20 14:52:58 +0100

    pass challanges from question 1 to 4

--- src/main/java/com/pharmacy/pharbrief/controllers/ProductController.java
+++ src/main/java/com/pharmacy/pharbrief/controllers/ProductController.java
@@ -2,7 +2,9 @@ package com.pharmacy.pharbrief.controllers;
 
 import com.pharmacy.pharbrief.dto.ProductDTO;
 import com.pharmacy.pharbrief.model.Product;
+import com.pharmacy.pharbrief.repository.ProductRepository;
 import com.pharmacy.pharbrief.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,6 +12,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+
+    @Autowired
+    ProductRepository productRepository;
 
     private final ProductService productService;
     public ProductController (final ProductService productService) {
@@ -44,9 +49,29 @@ public class ProductController {
         productService.deleteProduct(id);
     }
 
-    @GetMapping("/count/{price}")
-    public int countPriceProducts (@PathVariable Integer price) {
-        return productService.countProducts(price);
-    }
+//    @GetMapping("/count/{price}")
+//    public int countPriceProducts (@PathVariable Integer price) {
+//        return productService.countProducts(price);
+//    }
 
+//    @GetMapping("/find-product")
+//    public Product findByName (@RequestBody Product  product) {
+//        System.out.println(product);
+//        return productService.findProductByName(product.getName());
+//    }
+
+
+    @GetMapping("/greater-price/{prix}")
+
+    public  List<Product> greaterPrice(@PathVariable double prix){
+
+        return  productService.getPriceGreater(prix);
+
+    }
+    @GetMapping({"/Quantity-less-than/{quantity}", "/quantity/{quantity}"})
+
+    public List<Product> findByQuantityLessThan(@PathVariable int quantity){
+
+        return productService.getProductsWithQunatityLessThan(quantity);
+    }
 }
